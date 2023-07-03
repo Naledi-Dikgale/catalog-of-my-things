a36c7f79ad05283345eb3d49aed996b3cbf59b95
naledi-dikgale <naledi22khosi@gmail.com>
2023-07-03 16:32:52 +0200

    Add app and main.rb files

---
+++ app.rb
@@ -0,0 +1,14 @@
+class App
+  attr_accessor :books, :music_album, :games
+
+  def initialize
+    @books = []
+    @music_album = []
+    @games = []
+  end
+
+
+  include Games
+  include Book
+  include MusicAlbum
+end
---
+++ main.rb
@@ -0,0 +1,53 @@
+require_relative 'app'
+
+# basic inputs
+class Main
+  def select_options
+    puts 'Welcome to my app'
+    puts 'Please choose an option'
+    puts '1. Display all books'
+    puts '2. Display all music albums'
+    puts '3. Display all games'
+    puts '4. Add books'
+    puts '5. Add music album'
+    puts '6. Add games'
+    puts '7. Thats all for now'
+  end
+
+  def options(option)
+    app = App.new
+    case option
+    when '1'
+      app.display_books
+    when '2'
+      app.display_musical_album
+    when '3'
+      app.display_games
+    when '4'
+      app.add_book
+    when '5'
+      app.add_music_album
+    when '6'
+      app.add_game
+    end
+  end
+
+  def execute_app
+    loop do
+      select_options
+
+      option = gets.chomp
+
+      options(option)
+
+      break if option == '7'
+    end
+  end
+end
+
+def execute_main
+  main = Main.new
+  main.execute_app
+end
+
+execute_main
