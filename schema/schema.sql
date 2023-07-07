a29f7230bf2970f90d3873c3f14ab918eb27b034
Naledi <117156742+Naledi-Dikgale@users.noreply.github.com>
2023-07-07 02:23:19 -0700

    Update schema.sql

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,33 +1,76 @@
-CREATE TABLE music_albums (
-  id SERIAL PRIMARY KEY,
-  title VARCHAR(255) NOT NULL,
-  artist VARCHAR(255) NOT NULL,
-  release_year INT NOT NULL,
-  on_spotify BOOLEAN NOT NULL,
-  genre_id INT REFERENCES genres(id)
-);
+CREATE DATABASE catalog;
 
-CREATE TABLE genres (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(255) NOT NULL
-) ;
-
-CREATE TABLE games (
-  id SERIAL PRIMARY KEY,
-  multiplayer BOOLEAN,
-  last_played_at DATE,
+-- Create item table
+CREATE TABLE item (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   genre_id INT,
+  author_id INT,
   label_id INT,
   publish_date DATE,
-  author_id INT,
-  CONSTRAINT fk_genre FOREIGN KEY (genre_id) REFERENCES genres(genre_id),
-  CONSTRAINT fk_label FOREIGN KEY (label_id) REFERENCES label(label_id),
-  CONSTRAINT fk_author FOREIGN KEY (author_id) REFERENCES authors(author_id),
-  PRIMARY KEY(id)
+  archived BOOLEAN,
+  FOREIGN KEY (genre_id) REFERENCES genre(id),
+  FOREIGN KEY (author_id) REFERENCES author(id),
+  FOREIGN KEY (label_id) REFERENCES label(id)
 );
 
-CREATE TABLE authors (
-    id SERIAL PRIMARY KEY,
-    first_name  VARCHAR(100),
-    last_name   VARCHAR(100)
+-- Create genres table
+CREATE TABLE Genres (
+  id INT Generated ALWAYS AS IDENTITY PRIMARY KEY,
+  name VARCHAR(100),
 );
+
+-- Create music albums table
+CREATE TABLE MusicAlbums (
+  id INT Generated ALWAYS AS IDENTITY PRIMARY KEY,
+  publish_date DATE NOT NULL DEFAULT DATE,
+  on_spotify BOOLEAN,
+  archived BOOLEAN,
+  genre_id INT,
+  CONSTRAINT fk_genre FOREIGN KEY (genre_id) REFERENCES genres(id)
+);
+
+-- Create author table
+
+CREATE TABLE Author(
+    id int generated always as identity,
+    first_name varchar(255),
+    last_name varchar(255),
+    primary key (id)
+);
+
+-- Create game table
+
+CREATE TABLE Game(
+    id int generated always as identity,
+    multiplayer varchar(255),
+    last_played_at date,
+    published_date date,
+    archived boolean,
+    author_id int,
+    CONSTRAINT fk_author FOREIGN KEY (author_id) REFERENCES author(id),
+    primary key (id)
+);
+
+-- schema for labels
+CREATE TABLE Label (
+  id int generated always as identity,
+  title VARCHAR(255),
+  color VARCHAR(255)
+  publish_date date,
+  primary key (id)
+);
+
+-- create schema for book
+CREATE TABLE Books (
+  id Int generated always as identity,
+  publisher varchar(255),
+  cover_status varchar(255),
+  publish_date date,
+  archived boolean,
+  label_id Int,
+  CONSTRAINT fk_label FOREIGN KEY (label_id) REFERENCES label(id),
+  PRIMARY KEY (id)
+);
+
+CREATE INDEX genres ON MusicAlbums (genre_id);
+CREATE INDEX label_idx ON Books (label_id);
